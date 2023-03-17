20f84a5e7578b14e0233a845d21e885615ff001b
Gabriel Azevedo <gbldja@gmail.com>
2023-03-17 14:54:52 -0300

    test: component Main

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main/>', () => {
+  it('should render the reheading', () => {
+    const { container } = render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should contain background color', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
+  })
+})
